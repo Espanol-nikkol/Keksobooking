01410011a0fc3acc1ed75c43452d5c223eb83446
Espanol-nikkol <nikkol96@yandex.ru>
2019-06-27 06:34:16 +1000

    validation descr, address, type, price and time in-off

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
 'use strict';
 
+var KEYCODE_ENTER = 27;
+
 var typesFlats = ['palace', 'flat', 'house', 'bungalo'];
 var avatars = ['01', '02', '03', '04', '05', '06', '07', '08'];
 var ads = [];
+var fieldPrice = document.getElementById('price');
+var fieldType = document.getElementById('type');
+var mainPin = document.querySelector('.map__pin--main');
+var fieldTimeIn = document.getElementById('timein');
+var fieldTimeOut = document.getElementById('timeout');
 
 var getRandomNumber = function (min, max) {
   var randomNumber = Math.round(min - 0.5 + Math.random() * (max - min + 1));
@@ -20,6 +27,7 @@ var getAddress = function () {
   var address = document.getElementById('address');
   var mainPinX = parseFloat(mainPin.style.left);
   var mainPinY = parseFloat(mainPin.style.top);
+  address.value = mainPinX + 37 + ', ' + (mainPinY + 77);
   address.placeholder = mainPinX + 37 + ', ' + (mainPinY + 77);
 }
 
@@ -30,8 +38,15 @@ var activatedMap = function () {
     fields[i].disabled = false;
   }
   getAddress();
+  renderPins();
   document.querySelector('.ad-form').classList.remove('ad-form--disabled');
 };
+
+var onMainPinPressEnter = function (evt) {
+  if (evt.keycode === KEYCODE_ENTER) {
+    activatedMap();
+  }
+}
 
 var createdAds = function () {
   for (var i = 0; i < 8; i++) {
@@ -59,8 +74,38 @@ var renderPins = function () {
   document.querySelector('.map__pins').appendChild(fragment);
 };
 
+var validationTypesPrice = function () {
+  var selectedIndex = fieldType.options.selectedIndex;
+  if (fieldType.options[selectedIndex].value === 'flat') {
+    fieldPrice.min = '1000';
+    fieldPrice.placeholder = '1000';
+  } else if (fieldType.options[selectedIndex].value === 'house') {
+    fieldPrice.min = '5000';
+    fieldPrice.placeholder = '5000';
+  } else if (fieldType.options[selectedIndex].value === 'palace') {
+    fieldPrice.min = '10000';
+    fieldPrice.placeholder = '10000';
+  } else (
+    fieldPrice.min = '0',
+    fieldPrice.placeholder = '0'
+    )
+}
 
-var mainPin = document.querySelector('.map__pin--main');
-console.log(mainPin)
+var validationTimeIn = function () {
+  var selectedIndex = fieldTimeIn.options.selectedIndex;
+  fieldTimeOut.options[selectedIndex].selected = true;
+}
+
+var validationTimeOut = function () {
+  var selectedIndex = fieldTimeOut.options.selectedIndex;
+  fieldTimeIn.options[selectedIndex].selected = true;
+}
+
 mainPin.addEventListener('click', activatedMap);
+mainPin.addEventListener('keydown', onMainPinPressEnter);
 createdAds();
+fieldType.addEventListener('change', validationTypesPrice);
+fieldTimeIn.addEventListener('change', validationTimeIn);
+fieldTimeOut.addEventListener('change', validationTimeOut);
+document.addEventListener('invalid', function () {
+})
