2faeabdfca305032da7c280b3482123d90f21fce
Espanol-nikkol <nikkol96@yandex.ru>
2019-06-30 13:50:49 +1000

    created order and purity

--- js/mainpin.js
+++ js/mainpin.js
@@ -1,30 +1,30 @@
 'use strict';
 (function () {
-var startCoord = {};
-var mainPin = window.util.variable.mainPin;
-var consts = window.util.const
-var map = document.querySelector('.map')
+  var startCoord = {};
+  var mainPin = window.util.variable.mainPin;
+  var consts = window.util.const;
+  var map = document.querySelector('.map');
 
-  var onMainPinMouseDown = function(evt) {
-    evt.preventDefault()
+  var onMainPinMouseDown = function (evt) {
+    evt.preventDefault();
     startCoord = {
       x: evt.clientX,
       y: evt.clientY
-    }
-  }
+    };
+  };
 
-    var onMainPinMouseMove = function(evtMove) {
-      evtMove.preventDefault();
+  var onMainPinMouseMove = function (evtMove) {
+    evtMove.preventDefault();
 
     var shift = {
       x: (startCoord.x - evtMove.clientX),
       y: (startCoord.y - evtMove.clientY)
-    }
+    };
 
     startCoord = {
       x: evtMove.clientX,
       y: evtMove.clientY
-    }
+    };
 
     mainPin.style.left = (mainPin.offsetLeft - shift.x) + 'px';
     mainPin.style.top = (mainPin.offsetTop - shift.y) + 'px';
@@ -37,40 +37,40 @@ var map = document.querySelector('.map')
     }
     if (mainPin.offsetTop >= consts.BORDER_MAP_BOTTOM + 30) {
       map.removeEventListener('mousemove', onMainPinMouseMove);
-      var newPosition = parseFloat(mainPin.style.top) - 5;
+      newPosition = parseFloat(mainPin.style.top) - 5;
       newPosition = newPosition + 'px';
       mainPin.style.top = newPosition;
     }
     if (mainPin.offsetLeft <= (consts.BORDER_MAP_LEFT - 16)) {
       map.removeEventListener('mousemove', onMainPinMouseMove);
-      var newPosition = parseFloat(mainPin.style.left) + 5;
+      newPosition = parseFloat(mainPin.style.left) + 5;
       newPosition = newPosition + 'px';
       mainPin.style.left = newPosition;
     }
     if (mainPin.offsetLeft >= (consts.BORDER_MAP_RIGHT - 30)) {
       map.removeEventListener('mousemove', onMainPinMouseMove);
-      var newPosition = parseFloat(mainPin.style.left) - 5;
+      newPosition = parseFloat(mainPin.style.left) - 5;
       newPosition = newPosition + 'px';
       mainPin.style.left = newPosition;
     }
-  }
+  };
 
   var onMainPinMouseUp = function (evtUp) {
     evtUp.preventDefault();
     map.removeEventListener('mousemove', onMainPinMouseMove);
     document.removeEventListener('mouseup', onMainPinMouseUp);
-  }
+  };
 
   var activatedMap = function () {
-  document.querySelector('.map').classList.toggle('map--faded', false);
-  var fields = document.querySelectorAll('fieldset');
-  for (var i = 0; i < fields.length; i++) {
-    fields[i].disabled = false;
-  }
-  window.renderPins();
-  window.util.getAddress();
-  document.querySelector('.ad-form').classList.remove('ad-form--disabled');
-}
+    document.querySelector('.map').classList.toggle('map--faded', false);
+    var fields = document.querySelectorAll('fieldset');
+    for (var i = 0; i < fields.length; i++) {
+      fields[i].disabled = false;
+    }
+    window.renderPins();
+    window.util.getAddress();
+    document.querySelector('.ad-form').classList.remove('ad-form--disabled');
+  };
 
 
   mainPin.addEventListener('mousedown', function (evt) {
@@ -80,8 +80,7 @@ var map = document.querySelector('.map')
     document.addEventListener('mouseup', onMainPinMouseUp);
   });
   mainPin.addEventListener('keydown', function (evt) {
-  window.util.isEnterEvent(evt, window.cards.activatedMap)
-});  }
-
-
-) ()
+    window.util.isEnterEvent(evt, window.cards.activatedMap);
+  }
+  );
+}) ();
--- js/validation.js
+++ js/validation.js
@@ -16,22 +16,21 @@ var validationTypesPrice = function () {
   } else if (fieldType.options[selectedIndex].value === 'palace') {
     fieldPrice.min = '10000';
     fieldPrice.placeholder = '10000';
-  } else (
-    fieldPrice.min = '0',
-    fieldPrice.placeholder = '0'
-    )
-}
+  } else {
+    fieldPrice.min = '0';
+    fieldPrice.placeholder = '0';
+  }
+};
 
 var validationTimeIn = function () {
   var selectedIndex = fieldTimeIn.options.selectedIndex;
   fieldTimeOut.options[selectedIndex].selected = true;
-}
+};
 
 var validationTimeOut = function () {
   var selectedIndex = fieldTimeOut.options.selectedIndex;
   fieldTimeIn.options[selectedIndex].selected = true;
-}
-
+};
 
 fieldType.addEventListener('change', validationTypesPrice);
 fieldTimeIn.addEventListener('change', validationTimeIn);
