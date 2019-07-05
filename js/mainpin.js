f029bfafbefc42617a3219ca13a29914234df472
Espanol-nikkol <nikkol96@yandex.ru>
2019-07-05 10:42:09 +1000

    render cards from server

--- js/mainpin.js
+++ js/mainpin.js
@@ -14,53 +14,53 @@
     };
   };
 
-    var onMainPinMouseMove = function (evtMove) {
-      evtMove.preventDefault();
+  var onMainPinMouseMove = function (evtMove) {
+    evtMove.preventDefault();
 
-      var shift = {
-        x: (startCoord.x - evtMove.clientX),
-        y: (startCoord.y - evtMove.clientY)
-      };
-
-      startCoord = {
-        x: evtMove.clientX,
-        y: evtMove.clientY
-      };
-
-      mainPin.style.left = (mainPin.offsetLeft - shift.x) + 'px';
-      mainPin.style.top = (mainPin.offsetTop - shift.y) + 'px';
-      window.util.getAddress();
-      if (mainPin.offsetTop <= (consts.BORDER_MAP_TOP - 70)) {
-        map.removeEventListener('mousemove', onMainPinMouseMove);
-        var newPosition = parseFloat(mainPin.style.top) + 5;
-        newPosition = newPosition + 'px';
-        mainPin.style.top = newPosition;
-      }
-      if (mainPin.offsetTop >= consts.BORDER_MAP_BOTTOM + 30) {
-        map.removeEventListener('mousemove', onMainPinMouseMove);
-        newPosition = parseFloat(mainPin.style.top) - 5;
-        newPosition = newPosition + 'px';
-        mainPin.style.top = newPosition;
-      }
-      if (mainPin.offsetLeft <= (consts.BORDER_MAP_LEFT - 16)) {
-        map.removeEventListener('mousemove', onMainPinMouseMove);
-        newPosition = parseFloat(mainPin.style.left) + 5;
-        newPosition = newPosition + 'px';
-        mainPin.style.left = newPosition;
-      }
-      if (mainPin.offsetLeft >= (consts.BORDER_MAP_RIGHT - 30)) {
-        map.removeEventListener('mousemove', onMainPinMouseMove);
-        newPosition = parseFloat(mainPin.style.left) - 5;
-        newPosition = newPosition + 'px';
-        mainPin.style.left = newPosition;
-      }
+    var shift = {
+      x: (startCoord.x - evtMove.clientX),
+      y: (startCoord.y - evtMove.clientY)
     };
 
-    var onMainPinMouseUp = function (evtUp) {
-      evtUp.preventDefault();
+    startCoord = {
+      x: evtMove.clientX,
+      y: evtMove.clientY
+    };
+
+    mainPin.style.left = (mainPin.offsetLeft - shift.x) + 'px';
+    mainPin.style.top = (mainPin.offsetTop - shift.y) + 'px';
+    window.util.getAddress();
+    if (mainPin.offsetTop <= (consts.BORDER_MAP_TOP - 70)) {
       map.removeEventListener('mousemove', onMainPinMouseMove);
-      document.removeEventListener('mouseup', onMainPinMouseUp);
-    };
+      var newPosition = parseFloat(mainPin.style.top) + 5;
+      newPosition = newPosition + 'px';
+      mainPin.style.top = newPosition;
+    }
+    if (mainPin.offsetTop >= consts.BORDER_MAP_BOTTOM + 30) {
+      map.removeEventListener('mousemove', onMainPinMouseMove);
+      newPosition = parseFloat(mainPin.style.top) - 5;
+      newPosition = newPosition + 'px';
+      mainPin.style.top = newPosition;
+    }
+    if (mainPin.offsetLeft <= (consts.BORDER_MAP_LEFT - 16)) {
+      map.removeEventListener('mousemove', onMainPinMouseMove);
+      newPosition = parseFloat(mainPin.style.left) + 5;
+      newPosition = newPosition + 'px';
+      mainPin.style.left = newPosition;
+    }
+    if (mainPin.offsetLeft >= (consts.BORDER_MAP_RIGHT - 30)) {
+      map.removeEventListener('mousemove', onMainPinMouseMove);
+      newPosition = parseFloat(mainPin.style.left) - 5;
+      newPosition = newPosition + 'px';
+      mainPin.style.left = newPosition;
+    }
+  };
+
+  var onMainPinMouseUp = function (evtUp) {
+    evtUp.preventDefault();
+    map.removeEventListener('mousemove', onMainPinMouseMove);
+    document.removeEventListener('mouseup', onMainPinMouseUp);
+  };
 
 
   var activatedMap = function (card) {
@@ -87,4 +87,4 @@
     window.util.isEnterEvent(evt, activatedMap);
   }
   );
-}) ();
+})();
