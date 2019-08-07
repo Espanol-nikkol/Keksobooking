4c57a117cb060e1fde94aa72fa5bf8acb109c314
Espanol-nikkol <nikkol96@yandex.ru>
2019-08-07 13:56:02 +1000

    finished filtration

--- js/filtration.js
+++ js/filtration.js
@@ -5,113 +5,159 @@
   var priceFilter = document.getElementById('housing-price');
   var roomFilter = document.getElementById('housing-rooms');
   var guestFilter = document.getElementById('housing-guests');
-
-  var usedFilterTypes = false;
-  var usedFilterPrice = false;
-  var usedFilterRoom = false;
-  var usedFilterGuest = false;
+  var featuresFilter = document.getElementById('housing-features');
+  var cardFilter = [];
 
   var index = '';
   var houseTypeSelected = '';
   var roomSelected = '';
   var priceSelected = '';
   var guestSelected = '';
-  var cardFilter = [];
+  var featuresSelected = [];
+  var checks = [];
 
   var getHouseType = function (elem) {
-    usedFilterTypes = false
+    filters.house.flag = false;
     if (houseTypeSelected === 'any') {
       return true;
     }
-    usedFilterTypes = true;
+    filters.house.flag = true;
+
     return elem.offer.type === houseTypeSelected;
   };
 
   var getRoomCount = function (elem) {
-    usedFilterRoom = false;
+    filters.rooms.flag = false;
     if (roomSelected === 'any') {
       return true;
     }
-    usedFilterRoom = true;
+    filters.rooms.flag = true;
     return elem.offer.rooms === Number(roomSelected);
   }
 
   var getPrice = function (elem) {
-    usedFilterPrice = false;
+    filters.price.flag = false;
     if (priceSelected === 'any') {
       return true;
     }
-    usedFilterRoom = true;
-    return elem.offer.price === Number(priceSelected);
+    filters.price.flag = true;
+    var currentPrice = elem.offer.price;
+    switch (priceSelected) {
+      case 'low':
+      return currentPrice <= 10000;
+      break;
+      case 'middle':
+      return (currentPrice >= 10000 && currentPrice <= 50000);
+      break;
+      case 'high':
+      return currentPrice >= 50000;
+      break;
+    }
   }
 
   var getGuest = function (elem) {
-    usedFilterGuest = false;
+    filters.guests.flag = false;
     if (guestSelected === 'any') {
       return true;
     }
-    usedFilterGuest = true;
+    filters.guests.flag = true;
     return elem.offer.guests === Number(guestSelected);
   }
 
-  var isFilterUse = function (filter) {
-    switch (true) {
-      case usedFilterTypes:
-        cardFilter = window.card.filter(getHouseType);
-        cardFilter = cardFilter.filter(filter);
-      case usedFilterGuest:
-        cardFilter = window.card.filter(getGuest);
-        cardFilter = cardFilter.filter(filter);
-      case usedFilterRoom:
-        cardFilter = window.card.filter(getRoomCount);
-        cardFilter = cardFilter.filter(filter);
-      case usedFilterPrice:
-        cardFilter = window.card.filter(getPrice);
-        cardFilter = cardFilter.filter(filter);
-        break;
-      default:
-        cardFilter = window.card.filter(filter)
-    }
+  var getFeatures = function (elem) {
+    checks = document.querySelectorAll('.map__checkbox').forEach(function(elem) {
+      if (elem.checked) {
+        checks.push(elem.value)
+      }
+    })
+
   }
 
-  var onFilterOfHouseType = function () {
-    index = houseTypeFilter.options.selectedIndex;
-    houseTypeSelected = houseTypeFilter.options[index].value;
-    isFilterUse(getHouseType)
-    window.clearPins();
-    window.removePopUp();
-    window.renderPins(cardFilter);
-  };
+  var filters = {
+    house: {
+      name: 'houseType',
+      flag: false,
+      filter: function () {
+        this.flag = true;
 
-  var onFilterOfRooms = function () {
-    index = roomFilter.options.selectedIndex;
-    roomSelected = roomFilter.options[index].value;
-    isFilterUse(getRoomCount);
-    window.clearPins();
-    window.removePopUp();
-    window.renderPins(cardFilter);
-  }
+        index = houseTypeFilter.options.selectedIndex;
+        houseTypeSelected = houseTypeFilter.options[index].value;
+        window.cardFilter = window.cardFilter.filter(getHouseType);
+        if (this.flag) {
+          window.clearPins();
+          window.removePopUp();
+          window.renderPins(window.cardFilter);}
+        }
+      },
+      rooms: {
+        name: 'rooms',
+        flag: false,
+        filter: function () {
+          this.flag = true;
 
-  var onFilterOfPrice = function () {
-    index = priceFilter.options.selectedIndex;
-    priceSelected = priceFilter.options[index].value;
-    isFilterUse(getPrice);
-    window.clearPins();
-    window.removePopUp();
-    window.renderPins(cardFilter);
-  }  
+          index = roomFilter.options.selectedIndex;
+          roomSelected = roomFilter.options[index].value;
+          window.cardFilter = window.cardFilter.filter(getRoomCount);
+          if (this.flag) {
+            window.clearPins();
+            window.removePopUp();
+            window.renderPins(window.cardFilter);}
+          }
+        },
+        price: {
+          name: 'price',
+          flag: false,
+          filter: function () {
+            this.flag = true;
+            index = priceFilter.options.selectedIndex;
+            priceSelected = priceFilter.options[index].value;
+            window.cardFilter = window.cardFilter.filter(getPrice);
+            if (this.flag) {
+              window.clearPins();
+              window.removePopUp();
+              window.renderPins(window.cardFilter);}
+            }
+          },
+          guests: {
+            name: 'guests',
+            flag: false,
+            filter: function () {
+              this.flag = true;
+              index = guestFilter.options.selectedIndex;
+              guestSelected = guestFilter.options[index].value;
+              window.cardFilter = window.cardFilter.filter(getGuest);
+              if (this.flag) {
+                window.clearPins();
+                window.removePopUp();
+                window.renderPins(window.cardFilter)}
+              }
+            },
+            features: {
+              name: 'features',
+              flag: false,
+              filter: function () {
+                this.flag = true;
+                document.querySelectorAll('.map__checkbox').forEach(function(elem) {
+                  if (elem.checked) {
+                    window.cardFilter = window.cardFilter.filter(function(elem2) {
+                      return elem2.offer.features.includes(elem.value)
+                    })
+                  }
+                })
+                if (this.flag) {
+                  window.clearPins();
+                  window.removePopUp();
+                  window.renderPins(window.cardFilter)
+                }
+              }
+            }
+          }
 
-  var onFilterOfGuest= function () {
-    index = guestFilter.options.selectedIndex;
-    guestSelected = guestFilter.options[index].value;
-    isFilterUse(getGuest);
-    window.clearPins();
-    window.removePopUp();
-    window.renderPins(cardFilter);
-  }  
-
-  houseTypeFilter.addEventListener('change', onFilterOfHouseType);
-  roomFilter.addEventListener('change', onFilterOfRooms);
-  priceFilter.addEventListener('change', onFilterOfPrice);
-  guestFilter.addEventListener('change', onFilterOfGuest);
-})();
+          document.querySelector('.map__filters').addEventListener('change', function () {
+            window.cardFilter = window.card.slice();
+            window.debounce(filters.house.filter);
+            window.debounce(filters.rooms.filter);
+            window.debounce(filters.price.filter);
+            window.debounce(filters.features.filter)
+          })
+        })();
