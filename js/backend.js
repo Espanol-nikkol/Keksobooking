38949d45c957ff31217fe21833a4660ed8765515
Николай Колганов <42237828+Espanol-nikkol@users.noreply.github.com>
2020-09-09 13:17:32 +1000

    make download photos and avatar

--- js/backend.js
+++ js/backend.js
@@ -33,17 +33,17 @@
     var fragment = document.createDocumentFragment();
     var template = document.getElementById('pin').content.querySelector('.map__pin').cloneNode(true);
     arr
-    .slice(0, 5)
-    .forEach(function (elem) {
-      var pin = template.cloneNode(true);
-      pin.style.left = elem.location.x - sizeMainPin.WIDTH / 2 + 'px';
-      pin.style.top = elem.location.y - sizeMainPin.WIDTH + 'px';
-      pin.querySelector('img').src = elem.author.avatar;
-      pin.addEventListener('click', function () {
-        window.renderPopUp(elem);
+      .slice(0, 5)
+      .forEach(function (elem) {
+        var pin = template.cloneNode(true);
+        pin.style.left = elem.location.x - sizeMainPin.WIDTH / 2 + 'px';
+        pin.style.top = elem.location.y - sizeMainPin.WIDTH + 'px';
+        pin.querySelector('img').src = elem.author.avatar;
+        pin.addEventListener('click', function () {
+          window.renderPopUp(elem);
+        });
+        fragment.appendChild(pin);
       });
-      fragment.appendChild(pin);
-    });
     document.querySelector('.map__pins').appendChild(fragment);
   };
 
@@ -126,7 +126,7 @@
   var onSubmitClick = function () {
     var formData = new FormData(document.querySelector('.ad-form'));
     xhr = new XMLHttpRequest();
-    xhr.open('post', 'https://js.dump.academy/keksobooking');
+    xhr.open('post', 'https://javascript.pages.academy/keksobooking');
     xhr.send(formData);
 
     var onSuccess = function () {
@@ -167,15 +167,16 @@
       elem.disabled = false;
     });
   };
+
   var deactivatedMap = function () {
     document.querySelector('.map').classList.toggle('map--faded', true);
     turnOffFields();
     document.querySelectorAll('.map__pin')
-    .forEach(function (elem, index) {
-      if (index > 0) {
-        elem.remove();
-      }
-    });
+      .forEach(function (elem, index) {
+        if (index > 0) {
+          elem.remove();
+        }
+      });
     window.flagOfActivation = 1;
     mainPin.style.left = '571px';
     mainPin.style.top = '375px';
@@ -217,7 +218,37 @@
     }
   };
 
-  xhr.open('get', 'https://js.dump.academy/keksobooking/data');
+  var loadAvatar = function (evt) {
+    var fReader = new FileReader();
+    fReader.readAsDataURL(document.querySelector('#' + evt.target.id).files[0]);
+    fReader.onloadend = function (evt) {
+      document.querySelector(".ad-form-preview--header img").src = evt.target.result;
+    }
+  }
+
+  var loadPhotos = function (evt) {
+    var fReader = new FileReader();
+    fReader.readAsDataURL(document.querySelector('#' + evt.target.id).files[0]);
+    fReader.onloadend = function (evt) {
+      var img = document.createElement('img')
+      img.width = '40'
+      img.height = '44'
+      img.style.marginRight = '15px'
+
+      img.src = evt.target.result;
+      var wrapper = document.querySelector(".ad-form-preview--photos")
+      wrapper.appendChild(img)
+      console.log((wrapper.children.length/5))
+      if (wrapper.children.length % 5 != 0 && wrapper.children.length <= 5) {
+        wrapper.style.width = 70 + 55 * (wrapper.children.length-1) + 'px'
+      } else if (wrapper.children.length % 5 === 0) {
+        wrapper.style.height = 70 + 85* ~~(wrapper.children.length / 5) + 'px'
+      }
+      console.log(wrapper.style.width)
+    }
+  }
+
+  xhr.open('get', 'https://javascript.pages.academy/keksobooking/data');
   xhr.send();
   xhr.addEventListener('load', function () {
     if (xhr.status === 200) {
@@ -226,7 +257,6 @@
       onError();
     }
   });
-
   xhr.addEventListener('error', function () {
     onError();
   });
@@ -248,6 +278,9 @@
   fieldTimeIn.addEventListener('change', validationTimeIn);
   fieldTimeOut.addEventListener('change', validationTimeOut);
   fieldRooms.addEventListener('change', validationRoomsGuests);
+  document.querySelector('#avatar').addEventListener('change', loadAvatar);
+  document.querySelector('#images').addEventListener('change', loadPhotos);
+
   submitBtn.addEventListener('click', function (evt) {
     evt.preventDefault();
     onSubmitClick();
